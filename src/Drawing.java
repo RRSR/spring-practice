5bdc04f70b3946510276b3104d662565b02335de
Raj Rathore <Raj.Rathore@tatacommunications.com>
2017-08-06 17:23:49 +0530

    Passing a list of point beans to the triangle bean.

--- src/Drawing.java
+++ src/Drawing.java
@@ -1,14 +1,14 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class Drawing {
+
+class Drawing {
 
   public static void main(String[] args) {
 
     ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-    Triangle triangle = (Triangle) context.getBean("triangle-name ");
+    Triangle triangle = (Triangle) context.getBean("triangle");
     triangle.draw();
 
   }
-
 }
--- src/Triangle.java
+++ src/Triangle.java
@@ -1,38 +1,21 @@
-public class Triangle {
+import java.util.List;
 
-  private Point pointA;
-  private Point pointB;
-  private Point pointC;
+class Triangle {
 
-  public void draw(){
+  private List<Point> points;
+
+  public void draw() {
     System.out.println("The triangle is formed by the following 3 points");
-    System.out.println("Point A ( "+pointA.getX()+" , "+pointA.getY()+" )");
-    System.out.println("Point B ( "+pointB.getX()+" , "+pointB.getY()+" )");
-    System.out.println("Point C ( "+pointC.getX()+" , "+pointC.getY()+" )");
+    for (Point point : points) {
+      System.out.println("Point : ( " + point.getX() + " , " + point.getY() + " )");
+    }
   }
 
-  public Point getPointA() {
-    return pointA;
+  public List<Point> getPoints() {
+    return points;
   }
 
-  public void setPointA(Point pointA) {
-    this.pointA = pointA;
+  public void setPoints(List<Point> points) {
+    this.points = points;
   }
-
-  public Point getPointB() {
-    return pointB;
-  }
-
-  public void setPointB(Point pointB) {
-    this.pointB = pointB;
-  }
-
-  public Point getPointC() {
-    return pointC;
-  }
-
-  public void setPointC(Point pointC) {
-    this.pointC = pointC;
-  }
-
 }
